c41d8888d06630412903c05fc565ec4e96adb469
ff <1002248787@qq.com>
2019-12-03 09:24:32 +0800

    项目结构创建完成

--- gmall-admin-web/src/main/java/com/atguigu/gmall/admin/GmallAdminWebApplication.java
+++ gmall-admin-web/src/main/java/com/atguigu/gmall/admin/GmallAdminWebApplication.java
@@ -1,8 +1,11 @@
 package com.atguigu.gmall.admin;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableDubbo
 @SpringBootApplication
 public class GmallAdminWebApplication {
 
--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+
     @Test
     void contextLoads() {
+        Product byId = productService.getById(1);
+        System.out.println(byId.getName());
     }
 
 }
