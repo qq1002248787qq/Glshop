783bda04345ac8005a4dc88c2996e8b87d1200c4
ff <1002248787@qq.com>
2019-12-06 15:26:55 +0800

    商品模块

--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -2,6 +2,9 @@ package com.atguigu.gmall.pms;
 
 import com.atguigu.gmall.pms.entity.Product;
 import com.atguigu.gmall.pms.service.ProductService;
+import com.atguigu.gmall.vo.PageInfoVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +23,14 @@ class GmallPmsApplicationTests {
         Product byId = productService.getById(1);
         System.out.println(byId.getName());
     }
+
+   /* @Test
+    void test1(){
+        IPage<Product> page = productMapper.selectPage(new Page<Product>(productQueryParam.getPageNum(), productQueryParam.getPageSize()), null);
+
+        PageInfoVo pageInfoVo = new PageInfoVo(page.getTotal(),page.getPages(),page.getSize(),productQueryParam.getPageSize(),
+                page.getRecords(),page.getCurrent());
+
+    }*/
 
 }
--- gmall-ums/src/test/java/com/atguigu/gmall/ums/GmallUmsApplicationTests.java
+++ gmall-ums/src/test/java/com/atguigu/gmall/ums/GmallUmsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.atguigu.gmall.ums;
 
+import com.atguigu.gmall.ums.entity.Admin;
+import com.atguigu.gmall.ums.service.AdminService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GmallUmsApplicationTests {
 
+    @Autowired
+    private AdminService AdminService;
     @Test
     void contextLoads() {
+        Admin byId = AdminService.getById(3);
+        System.out.println(byId.getUsername());
     }
 
 }
